d97904b5ebf79033bd93229c28133cd62d7ed692
nejrabujak <nejra.bujak@stu.ssst.edu.ba>
2023-02-09 11:34:28 +0100

    Modified cors

--- src/main/java/com/childrenFirstBE/config/CorsConfiguration.java
+++ src/main/java/com/childrenFirstBE/config/CorsConfiguration.java
@@ -20,8 +20,7 @@ public class CorsConfiguration {
                 registry.addMapping("/**")
                         .allowedOrigins(allowedOrigins)
                         .allowedMethods("*")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
+                        .allowedHeaders("*");
             }
         };
     }
