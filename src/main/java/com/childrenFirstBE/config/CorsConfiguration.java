27481f40e919bb3b57e07d6f447c5ee47aeccb10
nejrabujak <nejra.bujak@stu.ssst.edu.ba>
2023-02-09 20:05:44 +0100

    Modified

--- src/main/java/com/childrenFirstBE/config/CorsConfiguration.java
+++ src/main/java/com/childrenFirstBE/config/CorsConfiguration.java
@@ -1,6 +1,5 @@
 package com.childrenFirstBE.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -11,22 +10,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 public class CorsConfiguration implements WebMvcConfigurer{
 
-    @Value("${service.ui.url}")
-    private String[] allowedOrigins;
-
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins(allowedOrigins)
-                        .allowedMethods("*")
-                        .allowedHeaders("*")
-                        .allowedHeaders("Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method",
-                                "Access-Control-Request-Headers")
-                        .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials")
-                        .allowCredentials(true);
+                        .allowedOrigins("https://childrenfirst.netlify.app")
+                        .allowedMethods("*");
             }
         };
     }
